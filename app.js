b98b1dd008c6b6acf334173a1c6dfc0ab638b2ef
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-05-19 23:47:26 +0200

    Add function for computers random choice

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello World"); 
+
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randomChoice = Math.floor(Math.random() * choices.length) + 1;
+    
+    if (randomChoice === 1) {
+        return "Rock";
+    } else if (randomChoice === 2) {
+        return "Paper";
+    } else
+        return "Scissors";
+};
+
+const computerChoice = getComputerChoice();
+console.log("Computer's choice", computerChoice);
