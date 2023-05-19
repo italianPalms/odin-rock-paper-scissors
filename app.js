e891990344a9a032f55c4f7246d826c47a2cc3b2
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-05-20 00:05:04 +0200

    Add function to play a round and define who wins

--- app.js
+++ app.js
@@ -13,3 +13,40 @@ function getComputerChoice() {
 
 const computerChoice = getComputerChoice();
 console.log("Computer's choice", computerChoice);
+
+const computerSelection = computerChoice;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+            return "It's a tie";
+        } else if (computerSelection === "Paper") {
+            return "Computer wins!";
+        } else {
+            return "Congratz, you win!";
+        }
+    }
+
+    else if (playerSelection === "Paper") {
+        if (computerSelection === "Paper") {
+            return "It's a tie";
+        } else if (computerSelection === "Scissors") {
+            return "Computer wins";
+        } else {
+            return "Congratz, you win!";
+        }
+    }
+
+    else if (playerSelection === "Scissors") {
+        if (computerSelection === "Scissors") {
+            return "It's a tie";
+        } else if (computerSelection === "Rock") {
+            return "Computer wins";
+        } else {
+            return "Congratz, you win!";
+        }
+    }
+}
+
+const playerSelection = "Scissors";
+console.log(playRound(playerSelection, computerSelection));
